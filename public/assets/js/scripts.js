f980ea132169154b31a4f4946174d2c8c057713b
GreenBayRules <sheshank@outlook.com>
2018-06-24 08:22:31 -0700

    Add Blog

---
+++ public/assets/js/scripts.js
@@ -0,0 +1,23 @@
+if (window.location.href ==  "https://mathattackerz.firebaseapp.com/") {
+  window.location.href = "https://mathttackerz.ga";
+}
+window.onload = function() {
+  if (localStorage.viewed != "true") {
+    swal("Love Math Attackerz?", "Go follow our blog! There you will get many updates on what's happening with the site! Follow the blog, Comment on posts, and share!!!", {
+      buttons: {
+        cancel: "Seen it, don't show again!",
+        catch: {
+          text: "Check it out!",
+          value: "yes",
+        },
+      },
+    }).then((value) => {
+      if (value == "yes") {
+        window.location.href = "https://mathattackerz.blogspot.com";
+      } else {
+        localStorage.viewed = "true";
+      }
+    });
+  }
+
+};
